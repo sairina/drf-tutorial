024f4291f1748253087faad3cd33deb1633a384f
sairina <sairina.merinotsui@gmail.com>
2021-04-07 11:06:49 -0700

    add in authentication to api and connect snippets to users

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
 
 
 # class SnippetSerializer(serializers.Serializer):
@@ -36,6 +37,17 @@ class SnippetSerializer(serializers.ModelSerializer):
     Replaces the commented out serializer with the ModelSerializer 
     (a shortcut for creating serializer classes)
     '''
+    # snippets assocatied with the user who created them, so now have to add here
+    owner = serializers.ReadOnlyField(source='owner.username')
+    
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner',]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
